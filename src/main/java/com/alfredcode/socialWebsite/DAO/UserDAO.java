96f32f0608cfbe4f000859f99dbda7a5a62ec64b
dmo575 <dmo5@protonmail.com>
2024-07-03 20:38:41 -0400

    WIP, started working on implementing the database.

--- src/main/java/com/alfredcode/socialWebsite/DAO/UserDAO.java
+++ src/main/java/com/alfredcode/socialWebsite/DAO/UserDAO.java
@@ -1,5 +1,15 @@
 package com.alfredcode.socialWebsite.DAO;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,6 +26,42 @@ public class UserDAO {
     public Database db = Database.getInstance();
 
     public UserModel addUser(UserModel userModel) {
+
+        try {
+
+            Properties protperties = new Properties();
+            String databaseIp = null;
+            String databasePort = null;
+            String databaseName = null;
+            String username = null;
+            String password = null;
+
+            try{
+                protperties.load(new FileInputStream("./sot"));
+                databaseIp = protperties.getProperty("database.ip");
+                databasePort = protperties.getProperty("database.port");
+                databaseName = protperties.getProperty("database.name");
+                username = protperties.getProperty("database.username");
+                password = protperties.getProperty("database.password");
+
+            }catch(IOException ex) {
+                logger.error("Damn son, no file found. What you smoking san");
+            }
+
+            Connection connection = DriverManager.getConnection(String.format("jdbc:mysql://%s:%s/%s", databaseIp, databasePort, databaseName), username, password);
+            //Connection connection = DriverManager.getConnection("jdbc:mysql://146.190.125.135:3306/social_blogpost", "dummy", "@l0h432Qq");
+
+            PreparedStatement prepStatement = connection.prepareStatement("INSERT INTO user(username, password) VALUES(?, ?)");
+            prepStatement.setString(1, userModel.getUsername());
+            prepStatement.setString(2, userModel.getPassword());
+
+            prepStatement.executeUpdate();
+
+        }
+        catch(SQLException ex) {
+            logger.error("ERR ->>>>> SQL EX" + ex.getMessage());
+        }
+        
 
         return db.addUser(userModel);
     }
